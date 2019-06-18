142faad309516fa3f2424ce4e72e9095921401b6
FlorianValery <flo_val@sfr.fr>
2019-06-18 09:37:28 +0200

    update class

--- tetris.py
+++ tetris.py
@@ -1,8 +1,21 @@
 import sys, pygame
-
+import random
 
 pygame.init()
 SIZE = [860, 800]
+
+
+class Block:
+
+    def __init__(self, color, x, y):
+        self.color = color
+        self.x = x
+        self.y = y
+    
+    def draw_rect(self, screen, block):
+        pygame.draw.rect(screen, block.color, [block.x, block.y, 35, 35])
+
+    
 
 
 class Game:
@@ -15,13 +28,15 @@ class Game:
         self.screen.fill((0,0,0))
         
 
+    
+
     def run(self):
 
         # Set up virtual time
         clock = pygame.time.Clock()
         time_elapsed = 0
         row = 0
-
+        
         while 1:
            
             # init clock
@@ -33,14 +48,32 @@ class Game:
                 if event.type == pygame.QUIT: sys.exit()            
         
             pygame.draw.rect(self.screen, (255,255,255), [250, 30, 350, 700])
-
+            
             # Every x seconde
             if time_elapsed > 800: 
                 # update pos of curr block (falling)
-                pygame.draw.rect(self.screen, (255,0,0), [355, 30 + (row * 35) , 35, 35])
-                pygame.draw.rect(self.screen, (255,0,0), [390, 30 + (row * 35), 35, 35])
-                pygame.draw.rect(self.screen, (255,0,0), [425, 30 + (row * 35), 35, 35])
-                pygame.draw.rect(self.screen, (255,0,0), [425, 65 + (row * 35), 35, 35])
+
+                #Init all the blocks / put here for now
+                blocks = [
+                    [
+                        Block((255,0,0), 355, 30 + (row * 35)),
+                        Block((255,0,0), 390, 30 + (row * 35)),
+                        Block((255,0,0), 425, 30 + (row * 35)),
+                        Block((255,0,0), 425, 65 + (row * 35))
+                    ],
+
+                    [
+                        Block((255,0,0), 355, 30 + (row * 35)),
+                        Block((255,0,0), 390, 30 + (row * 35)),
+                        Block((255,0,0), 425, 30 + (row * 35)),
+                        Block((255,0,0), 460, 30 + (row * 35))
+                    ]
+                ]
+
+                #randomly choose a block
+                for x in random.choice(blocks):
+                    x.draw_rect(self.screen, x)
+                
                 
                 # Reset time to 0
                 time_elapsed= 0 
